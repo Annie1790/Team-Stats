ed32f79fc9eda9949d073ba164fafbe0db28d4fc
Annie1790 <budayaniko@outlook.com>
2022-11-24 11:51:27 +0000

    practice

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const secondTask = document.querySelector("#secondTask");
 const secondTaskContent = document.createElement("h3");
 secondTaskContent.style.color = "blue";
 secondTaskContent.textContent = "Hey I'm blue!"
+secondTaskContent.addEventListener("mouseenter", function (e) {
+    e.target.textContent = "I secretly change!"
+})
 
 secondTask.appendChild(secondTaskContent)
 
@@ -22,4 +25,20 @@ thirdTaskContainer.appendChild(thirdTaskContentH1)
 const fourthTask = document.querySelector("#thirdTask");
 const fourthTaskContent = document.createElement("p");
 fourthTaskContent.textContent = "ME TOO!"
+
 fourthTask.appendChild(fourthTaskContent)
+
+
+/*
+const button = document.querySelector(".button")
+button.onclick = () => {
+    alert("hello world!")
+}
+*/
+
+
+const button = document.querySelector(".button");
+button.addEventListener('click', function (e) {
+    e.target.textContent = "I have changed!"
+    e.target.style.color = "blue"
+  });
